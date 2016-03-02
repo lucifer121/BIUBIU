0001587c3061bb0a5a9bc958776c7c94c7f2182d
lucifer <604324055@qq.com>
2016-03-02 10:50:15 +0800

    send biubiu

--- gen/com/biubiu/biubiu/R.java
+++ gen/com/biubiu/biubiu/R.java
@@ -1119,7 +1119,8 @@ containing a value of this type.
         public static final int shadow=0x7f02006e;
         public static final int showleft_selector=0x7f02006f;
         public static final int showright_selector=0x7f020070;
-        public static final int title_bar=0x7f020071;
+        public static final int textview_bg=0x7f020071;
+        public static final int title_bar=0x7f020072;
     }
     public static final class id {
         public static final int action_bar=0x7f050021;
@@ -1133,7 +1134,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f05001b;
         public static final int action_menu_presenter=0x7f05001c;
         public static final int action_mode_close_button=0x7f050029;
-        public static final int action_settings=0x7f050047;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f05002a;
         public static final int always=0x7f050010;
         public static final int beginning=0x7f050016;
@@ -1147,15 +1148,16 @@ containing a value of this type.
         public static final int end=0x7f050018;
         public static final int expand_activities_button=0x7f05002b;
         public static final int expanded_menu=0x7f050031;
+        public static final int flowlayout=0x7f050041;
         public static final int fullscreen=0x7f050003;
         public static final int home=0x7f050019;
         public static final int homeAsUp=0x7f05000a;
         public static final int icon=0x7f05002f;
-        public static final int id_iv_left=0x7f050041;
-        public static final int id_iv_right=0x7f050042;
-        public static final int id_left_menu_frame=0x7f050044;
-        public static final int id_listview_categories=0x7f050043;
-        public static final int id_right_menu_frame=0x7f050045;
+        public static final int id_iv_left=0x7f050044;
+        public static final int id_iv_right=0x7f050045;
+        public static final int id_left_menu_frame=0x7f050047;
+        public static final int id_listview_categories=0x7f050046;
+        public static final int id_right_menu_frame=0x7f050048;
         public static final int ifRoom=0x7f05000f;
         public static final int image=0x7f05002c;
         public static final int left=0x7f050000;
@@ -1185,10 +1187,12 @@ containing a value of this type.
         public static final int showCustom=0x7f05000c;
         public static final int showHome=0x7f050009;
         public static final int showTitle=0x7f05000b;
-        public static final int slidingmenumain=0x7f050046;
+        public static final int slidingmenumain=0x7f050049;
         public static final int split_action_bar=0x7f050023;
         public static final int submit_area=0x7f05003e;
         public static final int tabMode=0x7f050007;
+        public static final int textView1=0x7f050042;
+        public static final int textView2=0x7f050043;
         public static final int title=0x7f050030;
         public static final int top_action_bar=0x7f050025;
         public static final int up=0x7f050026;
@@ -1244,16 +1248,18 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int left_menu=0x7f030018;
-        public static final int left_menu_frame=0x7f030019;
-        public static final int right_menu=0x7f03001a;
-        public static final int right_menu_frame=0x7f03001b;
-        public static final int slidingmenumain=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int activity_biu_biu_send=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int left_menu=0x7f030019;
+        public static final int left_menu_frame=0x7f03001a;
+        public static final int right_menu=0x7f03001b;
+        public static final int right_menu_frame=0x7f03001c;
+        public static final int slidingmenumain=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int biu_biu_send=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1299,6 +1305,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000f;
+        public static final int title_activity_biu_biu_send=0x7f0a0010;
     }
     public static final class style {
         /** 
---
+++ src/com/biubiu/biubiu/BiuBiuSendActivity.java
@@ -0,0 +1,61 @@
+package com.biubiu.biubiu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BiuBiuSendActivity extends Activity {
+	
+	 private String mNames[] = {
+	            "welcome","android","TextView",
+	            "apple","jamy","kobe bryant",
+	            "jordan","layout","viewgroup",
+	            "margin","padding","text",
+	            "name","type","search","logcat"
+	            
+	    };
+	    private Flowlayout mFlowLayout;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_biu_biu_send);
+		initChildViews();
+        
+    }
+    @SuppressWarnings("deprecation")
+	private void initChildViews() {
+        // TODO Auto-generated method stub
+        mFlowLayout = (Flowlayout) findViewById(R.id.flowlayout);
+        MarginLayoutParams lp = new MarginLayoutParams(
+                LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        lp.leftMargin = 5;
+        lp.rightMargin = 5;
+        lp.topMargin = 5;
+        lp.bottomMargin = 5;
+        for(int i = 0; i < mNames.length; i ++){
+            final TextView view = new TextView(this);
+            
+            view.setText(mNames[i]);
+            view.setTextColor(Color.WHITE);
+            view.setTextSize(16);
+            view.setBackgroundDrawable(getResources().getDrawable(R.drawable.textview_bg));
+            view.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub
+					Toast.makeText(getApplicationContext(), view.getText(), Toast.LENGTH_SHORT).show();
+				}
+			});
+            mFlowLayout.addView(view,lp);
+        }
+    }
+ 
+}
