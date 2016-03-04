4985789b0261a19703e114d2dd63fe710ddde9d2
miracleAi <lsfx1015@sina.com>
2016-03-04 12:25:27 +0800

    biu send click

--- src/com/android/biubiu/fragment/BiuFragment.java
+++ src/com/android/biubiu/fragment/BiuFragment.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
 
+import com.android.biubiu.activity.BiuBiuSendActivity;
 import com.android.biubiu.bean.DotBean;
 import com.android.biubiu.bean.UserBean;
 import com.android.biubiu.utils.BiuUtil;
@@ -18,6 +19,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -37,6 +39,9 @@ import android.widget.RelativeLayout.LayoutParams;
 @SuppressLint("NewApi")
 public class BiuFragment extends Fragment {
 	View view;
+	//启动发biubiu页面的requestcode
+	public static final int SEND_BIU_REQUEST = 1001;
+	//屏幕宽高
 	int width = 0;
 	int height = 0;
 	Handler handler;
@@ -169,15 +174,10 @@ public class BiuFragment extends Fragment {
 					userGif.setVisibility(View.GONE);
 				}*/
 				if(currentTime > 0){
-					Toast.makeText(getActivity(), "no click", 1).show();
 					return;
 				}
-				Toast.makeText(getActivity(), "click", 1).show();
-				currentTime = totalTime;
-				taskView.setVisibility(View.VISIBLE);
-				userBiuImv.setVisibility(View.GONE);
-				taskView.updeteTask(currentTime);
-				handler.post(r);
+				Intent intent = new Intent(getActivity(),BiuBiuSendActivity.class);
+				startActivityForResult(intent, SEND_BIU_REQUEST);
 			}
 		});
 	}
@@ -203,222 +203,222 @@ public class BiuFragment extends Fragment {
 		}
 	};
 	//放置接收到的用户
-		private void initUserGroup() {
-			//给三个头像layout赋值
-			userD1 = width/8;
-			userD2 = width/10;
-			userD3 = width*3/40;
-			//计算分割区域
-			c1DotList.clear();
-			c1DotList.addAll(BiuUtil.caculateCircle(n1, circleR1, x0, y0)) ;
-			c2DotList.clear();
-			c2DotList.addAll(BiuUtil.caculateCircle(n2, circleR2, x0, y0));
-			edgeAngleList.clear();
-			edgeAngleList.addAll(BiuUtil.getEdgeAngle(width, userD3, circleR3, x0, y0));
-			c3DotList.clear();
-			//将角度按照从小到大排列
-			Collections.sort(edgeAngleList, new SorByAngle());
-			c3DotList.addAll(BiuUtil.caculateCircleBig(edgeAngleList, userD3, circleR3, x0, y0, n3));
+	private void initUserGroup() {
+		//给三个头像layout赋值
+		userD1 = width/8;
+		userD2 = width/10;
+		userD3 = width*3/40;
+		//计算分割区域
+		c1DotList.clear();
+		c1DotList.addAll(BiuUtil.caculateCircle(n1, circleR1, x0, y0)) ;
+		c2DotList.clear();
+		c2DotList.addAll(BiuUtil.caculateCircle(n2, circleR2, x0, y0));
+		edgeAngleList.clear();
+		edgeAngleList.addAll(BiuUtil.getEdgeAngle(width, userD3, circleR3, x0, y0));
+		c3DotList.clear();
+		//将角度按照从小到大排列
+		Collections.sort(edgeAngleList, new SorByAngle());
+		c3DotList.addAll(BiuUtil.caculateCircleBig(edgeAngleList, userD3, circleR3, x0, y0, n3));
+	}
+	//按照角度
+	class SorByAngle implements Comparator {
+		public int compare(Object o1, Object o2) {
+			Double s1 = (Double) o1;
+			Double s2 = (Double) o2;
+			if (s1 < s2)
+				return 1;
+			return 0;
 		}
-		//按照角度
-		class SorByAngle implements Comparator {
-			public int compare(Object o1, Object o2) {
-				Double s1 = (Double) o1;
-				Double s2 = (Double) o2;
-				if (s1 < s2)
-					return 1;
-				return 0;
-			}
-		}
-		//往第一个圈上放view
-		private void addCircle1View(UserBean userBean) {
-			boolean haveSpace = false;
-			for(int i=0;i<n1;i++){
-				DotBean bean = c1DotList.get(i);
-				if(!bean.isAdd()){
-					//添加区域标记
-					userBean.setIndex(i);
-					haveSpace = true;
-					//计算放置位置
-					double randomAngle = BiuUtil.getRandomAngle(n1, i, userD1, circleR1);
-					int xLocation =  BiuUtil.getLocationX(randomAngle, userD1, circleR1, x0);
-					int yLocation = BiuUtil.getLocationY(randomAngle, userD1, circleR1, y0);
-					userBean.setX(xLocation);
-					userBean.setY(yLocation);
-					createCir1NewView(xLocation, yLocation, (int)userD1, (int)userD1, userBean);
-					c1DotList.get(i).setAdd(true);
-					break ;
-				}
-			}
-			if(!haveSpace){
-				//将内圈中的用户按照时间顺序排列
-				Collections.sort(user1List,new SorByTime());
-				UserBean moveBean = user1List.get(0);
-				user1List.remove(0);
-				moveOneToTwo(moveBean);
+	}
+	//往第一个圈上放view
+	private void addCircle1View(UserBean userBean) {
+		boolean haveSpace = false;
+		for(int i=0;i<n1;i++){
+			DotBean bean = c1DotList.get(i);
+			if(!bean.isAdd()){
+				//添加区域标记
+				userBean.setIndex(i);
+				haveSpace = true;
+				//计算放置位置
+				double randomAngle = BiuUtil.getRandomAngle(n1, i, userD1, circleR1);
+				int xLocation =  BiuUtil.getLocationX(randomAngle, userD1, circleR1, x0);
+				int yLocation = BiuUtil.getLocationY(randomAngle, userD1, circleR1, y0);
+				userBean.setX(xLocation);
+				userBean.setY(yLocation);
+				createCir1NewView(xLocation, yLocation, (int)userD1, (int)userD1, userBean);
+				c1DotList.get(i).setAdd(true);
+				break ;
 			}
 		}
-		//从第1个圈移到第2个圈
-		private void moveOneToTwo(UserBean oneUserBean) {
-			boolean haveSpace = false;
-			for(int i=0;i<n2;i++){
-				DotBean dotBean = c2DotList.get(i);
-				if(!dotBean.isAdd()){
-					haveSpace = true;
-					//内圈最早的移到第二圈,计算移到第二圈的位置x2,y2
-					double randomAngle = BiuUtil.getRandomAngle(n2, i, userD2, circleR2);
-					int x2 = BiuUtil.getLocationX(randomAngle, userD2, circleR2, x0);
-					int y2 = BiuUtil.getLocationY(randomAngle, userD2, circleR2, y0);
-					moveUserView(oneUserBean.getX(), oneUserBean.getY(),
-							x2, y2,oneUserBean,userD2,userD2/userD1);
-					oneUserBean.setX(x2);
-					oneUserBean.setY(y2);
-					c2DotList.get(i).setAdd(true);
-					//改变空闲标记，将最新插入内圈
-					c1DotList.get(oneUserBean.getIndex()).setAdd(false);
-					//移动后改变区域标记
-					oneUserBean.setIndex(i);
-					user2List.add(oneUserBean);
-					addCircle1View(newUserBean);
-					break;
-				}
-			}
-			if(!haveSpace){
-				Collections.sort(user2List,new SorByTime());
-				UserBean twoUserBean = user2List.get(0);
-				user2List.remove(0);
-				moveTwoToThree(oneUserBean,twoUserBean);
+		if(!haveSpace){
+			//将内圈中的用户按照时间顺序排列
+			Collections.sort(user1List,new SorByTime());
+			UserBean moveBean = user1List.get(0);
+			user1List.remove(0);
+			moveOneToTwo(moveBean);
+		}
+	}
+	//从第1个圈移到第2个圈
+	private void moveOneToTwo(UserBean oneUserBean) {
+		boolean haveSpace = false;
+		for(int i=0;i<n2;i++){
+			DotBean dotBean = c2DotList.get(i);
+			if(!dotBean.isAdd()){
+				haveSpace = true;
+				//内圈最早的移到第二圈,计算移到第二圈的位置x2,y2
+				double randomAngle = BiuUtil.getRandomAngle(n2, i, userD2, circleR2);
+				int x2 = BiuUtil.getLocationX(randomAngle, userD2, circleR2, x0);
+				int y2 = BiuUtil.getLocationY(randomAngle, userD2, circleR2, y0);
+				moveUserView(oneUserBean.getX(), oneUserBean.getY(),
+						x2, y2,oneUserBean,userD2,userD2/userD1);
+				oneUserBean.setX(x2);
+				oneUserBean.setY(y2);
+				c2DotList.get(i).setAdd(true);
+				//改变空闲标记，将最新插入内圈
+				c1DotList.get(oneUserBean.getIndex()).setAdd(false);
+				//移动后改变区域标记
+				oneUserBean.setIndex(i);
+				user2List.add(oneUserBean);
+				addCircle1View(newUserBean);
+				break;
 			}
 		}
-		//从第2个圈移到第3个圈
-		private void moveTwoToThree(UserBean oneUserBean,UserBean twoUserBean) {
-			boolean haveSpace = false;
-			for(int i=0;i<n3;i++){
-				DotBean dotBean = c3DotList.get(i);
-				if(!dotBean.isAdd()){
-					haveSpace = true;
-					//从第2个圈移到第3个圈,计算移到第二圈的位置x3,y3
-					//将角度按照从小到大排列
-					Collections.sort(edgeAngleList, new SorByAngle());
-					double randomAngle = BiuUtil.getRandomAngleBig(edgeAngleList, n3, i, userD1, circleR3);
-					int x3 = BiuUtil.getLocationX(randomAngle, userD3, circleR3, x0);
-					int y3 = BiuUtil.getLocationY(randomAngle, userD3, circleR3, y0);
-					moveUserView(twoUserBean.getX(), twoUserBean.getY(),
-							x3, y3,twoUserBean,userD3,userD3/userD1);
-					twoUserBean.setX(x3);
-					twoUserBean.setY(y3);
-					c3DotList.get(i).setAdd(true);
-					//改变空闲标记，内圈移到第二圈
-					c2DotList.get(twoUserBean.getIndex()).setAdd(false);
-					//移动后改变区域标记
-					twoUserBean.setIndex(i);
-					user3List.add(twoUserBean);
-					moveOneToTwo(oneUserBean);
-					break;
-				}
-			}
-			if(!haveSpace){
-				//移除外圈最早的
-				Collections.sort(user3List,new SorByTime());
-				UserBean delBean = user3List.get(0);
-				RelativeLayout rl = (RelativeLayout) userGroupLayout.findViewById(Integer.valueOf(1+delBean.getId()));
-				userGroupLayout.removeView(rl);
-				//改变空闲标记，第二圈移到外圈
-				c3DotList.get(delBean.getIndex()).setAdd(false);
-				user3List.remove(0);
-				moveTwoToThree(oneUserBean,twoUserBean);
+		if(!haveSpace){
+			Collections.sort(user2List,new SorByTime());
+			UserBean twoUserBean = user2List.get(0);
+			user2List.remove(0);
+			moveTwoToThree(oneUserBean,twoUserBean);
+		}
+	}
+	//从第2个圈移到第3个圈
+	private void moveTwoToThree(UserBean oneUserBean,UserBean twoUserBean) {
+		boolean haveSpace = false;
+		for(int i=0;i<n3;i++){
+			DotBean dotBean = c3DotList.get(i);
+			if(!dotBean.isAdd()){
+				haveSpace = true;
+				//从第2个圈移到第3个圈,计算移到第二圈的位置x3,y3
+				//将角度按照从小到大排列
+				Collections.sort(edgeAngleList, new SorByAngle());
+				double randomAngle = BiuUtil.getRandomAngleBig(edgeAngleList, n3, i, userD1, circleR3);
+				int x3 = BiuUtil.getLocationX(randomAngle, userD3, circleR3, x0);
+				int y3 = BiuUtil.getLocationY(randomAngle, userD3, circleR3, y0);
+				moveUserView(twoUserBean.getX(), twoUserBean.getY(),
+						x3, y3,twoUserBean,userD3,userD3/userD1);
+				twoUserBean.setX(x3);
+				twoUserBean.setY(y3);
+				c3DotList.get(i).setAdd(true);
+				//改变空闲标记，内圈移到第二圈
+				c2DotList.get(twoUserBean.getIndex()).setAdd(false);
+				//移动后改变区域标记
+				twoUserBean.setIndex(i);
+				user3List.add(twoUserBean);
+				moveOneToTwo(oneUserBean);
+				break;
 			}
 		}
-		//按照时间排序
-		class SorByTime implements Comparator {
-			public int compare(Object o1, Object o2) {
-				UserBean s1 = (UserBean) o1;
-				UserBean s2 = (UserBean) o2;
-				if (s1.getTime() < s2.getTime())
-					return 1;
-				return 0;
-			}
+		if(!haveSpace){
+			//移除外圈最早的
+			Collections.sort(user3List,new SorByTime());
+			UserBean delBean = user3List.get(0);
+			RelativeLayout rl = (RelativeLayout) userGroupLayout.findViewById(Integer.valueOf(1+delBean.getId()));
+			userGroupLayout.removeView(rl);
+			//改变空闲标记，第二圈移到外圈
+			c3DotList.get(delBean.getIndex()).setAdd(false);
+			user3List.remove(0);
+			moveTwoToThree(oneUserBean,twoUserBean);
 		}
-		//创建第一圈上新的view
-		private void createCir1NewView(int xLocation,int yLocation,int lWidth,int lHeight,UserBean bean){
-			user1List.add(bean);
-			final RelativeLayout rl = new RelativeLayout(getActivity());
-			rl.setId(Integer.valueOf(1+bean.getId()));
-			AbsoluteLayout.LayoutParams llParams = new AbsoluteLayout.LayoutParams(
-					lWidth,
-					lHeight, xLocation, yLocation);
-			final GifView gifView = new GifView(getActivity());
-			RelativeLayout.LayoutParams gifP = new RelativeLayout.LayoutParams(
-					lWidth,
-					lHeight);
-			gifView.setGifImage(R.drawable.anim);
-			gifView.setShowDimension(lWidth, lHeight);
-			gifView.setGifImageType(GifImageType.COVER);
-			rl.addView(gifView, gifP);
-			ImageView imageView = new ImageView(getActivity());
-			RelativeLayout.LayoutParams imageP = new RelativeLayout.LayoutParams(
-					lWidth-35,
-					lHeight-35);
-			imageView.setId(Integer.valueOf(2+bean.getId()));
-			imageP.addRule(RelativeLayout.CENTER_IN_PARENT); 
-			imageView.setImageResource(R.drawable.chat_img_profiles_default);
-			rl.addView(imageView, imageP);
-			
-			final ImageView imageViewL = new ImageView(getActivity());
-			RelativeLayout.LayoutParams imagePL = new RelativeLayout.LayoutParams(
-					20,
-					20);
-			imageViewL.setId(Integer.valueOf(4+bean.getId()));
-			imagePL.addRule(RelativeLayout.ALIGN_RIGHT,imageView.getId());
-			imagePL.addRule(RelativeLayout.ALIGN_BOTTOM,imageView.getId());
-			imagePL.leftMargin = 5;
-			imageViewL.setImageResource(R.drawable.new_dot);
-			rl.addView(imageViewL, imagePL);
-			
-			TextView tv = new TextView(getActivity());
-			RelativeLayout.LayoutParams tvP = new RelativeLayout.LayoutParams(LayoutParams .WRAP_CONTENT,LayoutParams .WRAP_CONTENT);
-			tvP.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM); 
-			tvP.addRule(RelativeLayout.CENTER_HORIZONTAL); 
-			tv.setText("11%");
-			tv.setMaxLines(1);
-			tv.setTextSize(8);
-			tv.setGravity(Gravity.CENTER);
-			tv.setTextColor(getResources().getColor(R.color.white));
-			tv.setId(Integer.valueOf(3+bean.getId()));
-			rl.addView(tv, tvP);
-			userGroupLayout.addView(rl, llParams);
-			new Handler().postDelayed(new Runnable() {
+	}
+	//按照时间排序
+	class SorByTime implements Comparator {
+		public int compare(Object o1, Object o2) {
+			UserBean s1 = (UserBean) o1;
+			UserBean s2 = (UserBean) o2;
+			if (s1.getTime() < s2.getTime())
+				return 1;
+			return 0;
+		}
+	}
+	//创建第一圈上新的view
+	private void createCir1NewView(int xLocation,int yLocation,int lWidth,int lHeight,UserBean bean){
+		user1List.add(bean);
+		final RelativeLayout rl = new RelativeLayout(getActivity());
+		rl.setId(Integer.valueOf(1+bean.getId()));
+		AbsoluteLayout.LayoutParams llParams = new AbsoluteLayout.LayoutParams(
+				lWidth,
+				lHeight, xLocation, yLocation);
+		final GifView gifView = new GifView(getActivity());
+		RelativeLayout.LayoutParams gifP = new RelativeLayout.LayoutParams(
+				lWidth,
+				lHeight);
+		gifView.setGifImage(R.drawable.anim);
+		gifView.setShowDimension(lWidth, lHeight);
+		gifView.setGifImageType(GifImageType.COVER);
+		rl.addView(gifView, gifP);
+		ImageView imageView = new ImageView(getActivity());
+		RelativeLayout.LayoutParams imageP = new RelativeLayout.LayoutParams(
+				lWidth-35,
+				lHeight-35);
+		imageView.setId(Integer.valueOf(2+bean.getId()));
+		imageP.addRule(RelativeLayout.CENTER_IN_PARENT); 
+		imageView.setImageResource(R.drawable.chat_img_profiles_default);
+		rl.addView(imageView, imageP);
 
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					rl.removeView(gifView);
-				}
-			}, 2000);
-			rl.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-					imageViewL.setVisibility(View.GONE);
-				}
-			});
-		}
-		//移动view
-		public void moveUserView(double startX,double startY,double endX,double endY,UserBean userBean,float viewD,float scale){
-			final RelativeLayout rl = (RelativeLayout) userGroupLayout.findViewById(Integer.valueOf(1+userBean.getId()));
-			float d = viewD*(1-scale)/2;
-			ObjectAnimator anim1 = ObjectAnimator.ofFloat(rl, "x", (float)startX,(float)(endX-d));
-			ObjectAnimator anim2 = ObjectAnimator.ofFloat(rl, "y", (float)startY,(float)(endY-d));
-			ObjectAnimator anim3 = ObjectAnimator.ofFloat(rl, "scaleX",  1.0f, scale);  
-	        ObjectAnimator anim4 = ObjectAnimator.ofFloat(rl, "scaleY",   1.0f, scale);  
-			AnimatorSet animSet = new AnimatorSet();
-			animSet.setDuration(500);
-			animSet.playTogether(anim1, anim2,anim3,anim4);
-			animSet.start();
-			TextView tv = (TextView) rl.findViewById(Integer.valueOf(3+userBean.getId()));
-			tv.setVisibility(View.GONE);
-		}
+		final ImageView imageViewL = new ImageView(getActivity());
+		RelativeLayout.LayoutParams imagePL = new RelativeLayout.LayoutParams(
+				20,
+				20);
+		imageViewL.setId(Integer.valueOf(4+bean.getId()));
+		imagePL.addRule(RelativeLayout.ALIGN_RIGHT,imageView.getId());
+		imagePL.addRule(RelativeLayout.ALIGN_BOTTOM,imageView.getId());
+		imagePL.leftMargin = 5;
+		imageViewL.setImageResource(R.drawable.new_dot);
+		rl.addView(imageViewL, imagePL);
+
+		TextView tv = new TextView(getActivity());
+		RelativeLayout.LayoutParams tvP = new RelativeLayout.LayoutParams(LayoutParams .WRAP_CONTENT,LayoutParams .WRAP_CONTENT);
+		tvP.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM); 
+		tvP.addRule(RelativeLayout.CENTER_HORIZONTAL); 
+		tv.setText("11%");
+		tv.setMaxLines(1);
+		tv.setTextSize(8);
+		tv.setGravity(Gravity.CENTER);
+		tv.setTextColor(getResources().getColor(R.color.white));
+		tv.setId(Integer.valueOf(3+bean.getId()));
+		rl.addView(tv, tvP);
+		userGroupLayout.addView(rl, llParams);
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				rl.removeView(gifView);
+			}
+		}, 2000);
+		rl.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				imageViewL.setVisibility(View.GONE);
+			}
+		});
+	}
+	//移动view
+	public void moveUserView(double startX,double startY,double endX,double endY,UserBean userBean,float viewD,float scale){
+		final RelativeLayout rl = (RelativeLayout) userGroupLayout.findViewById(Integer.valueOf(1+userBean.getId()));
+		float d = viewD*(1-scale)/2;
+		ObjectAnimator anim1 = ObjectAnimator.ofFloat(rl, "x", (float)startX,(float)(endX-d));
+		ObjectAnimator anim2 = ObjectAnimator.ofFloat(rl, "y", (float)startY,(float)(endY-d));
+		ObjectAnimator anim3 = ObjectAnimator.ofFloat(rl, "scaleX",  1.0f, scale);  
+		ObjectAnimator anim4 = ObjectAnimator.ofFloat(rl, "scaleY",   1.0f, scale);  
+		AnimatorSet animSet = new AnimatorSet();
+		animSet.setDuration(500);
+		animSet.playTogether(anim1, anim2,anim3,anim4);
+		animSet.start();
+		TextView tv = (TextView) rl.findViewById(Integer.valueOf(3+userBean.getId()));
+		tv.setVisibility(View.GONE);
+	}
 	private void initTestBtn() {
 		// TODO Auto-generated method stub
 		testBtn.setOnClickListener(new OnClickListener() {
@@ -453,5 +453,23 @@ public class BiuFragment extends Fragment {
 		// TODO Auto-generated method stub
 		super.onPause();
 		handler.removeCallbacks(r);
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case SEND_BIU_REQUEST:
+			//开始倒计时
+			currentTime = totalTime;
+			taskView.setVisibility(View.VISIBLE);
+			userBiuImv.setVisibility(View.GONE);
+			taskView.updeteTask(currentTime);
+			handler.post(r);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
