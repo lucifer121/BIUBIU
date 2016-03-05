b77877ac350bb93522795191caa41dae153d28e1
miracleAi <lsfx1015@sina.com>
2016-03-05 11:59:49 +0800

    new

---
+++ src/com/android/biubiu/myapplication/BiubiuApplication.java
@@ -0,0 +1,11 @@
+package com.android.biubiu.myapplication;
+
+import android.app.Application;
+
+public class BiubiuApplication extends Application{
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+}
