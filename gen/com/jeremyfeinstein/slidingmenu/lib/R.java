ec32fb05bc83550346ef5cde543840452cebdb1f
lucifer <604324055@qq.com>
2016-03-17 17:13:35 +0800

    ui

--- gen/com/jeremyfeinstein/slidingmenu/lib/R.java
+++ gen/com/jeremyfeinstein/slidingmenu/lib/R.java
@@ -29,7 +29,7 @@ public final class R {
 		public static final int margin = 0x7f060004;
 		public static final int right = 0x7f060003;
 		public static final int selected_view = 0x7f060006;
-		public static final int slidingmenumain = 0x7f060176;
+		public static final int slidingmenumain = 0x7f060177;
 	}
 	public static final class layout {
 		public static final int slidingmenumain = 0x7f03005c;
