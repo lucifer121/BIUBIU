ce83ab3c412d4f97d923bb2a9bb8110a435e9d70
miracleAi <lsfx1015@sina.com>
2016-04-07 12:43:28 +0800

    db commit

--- src/com/android/biubiu/sqlite/MySqliteDBHelper.java
+++ src/com/android/biubiu/sqlite/MySqliteDBHelper.java
@@ -51,19 +51,25 @@ public class MySqliteDBHelper extends SQLiteOpenHelper {
 		user.append(DbConstents.USER_FRIEND_AGE + " Integer ");
 		user.append(")");
 		db.execSQL(user.toString());
-		
-		StringBuffer push = new StringBuffer();
-		push.append("create table if not exists ");
-		push.append(DbConstents.PUSH_MATCH + "(");
-		push.append(DbConstents.PUSH_USER_CODE + " varchar(100) primary key,");
-		push.append(DbConstents.PUSH_TIME + " Integer ");
-		push.append(")");
-		db.execSQL(push.toString());
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
 		// TODO Auto-generated method stub
+		switch (db.getVersion()) {
+		case 1:
+			
+		case 2:
+			StringBuffer push = new StringBuffer();
+			push.append("create table if not exists ");
+			push.append(DbConstents.PUSH_MATCH + "(");
+			push.append(DbConstents.PUSH_USER_CODE + " varchar(100) primary key,");
+			push.append(DbConstents.PUSH_TIME + " Integer ");
+			push.append(")");
+			db.execSQL(push.toString());
+		default:
+			break;
+		}
 		onCreate(db);
 	}
 
